eae8ece95c002049c1ff844ce4c076807cb99ee0
dollytiwari <dollytiwari393@gmail.com>
2022-09-12 01:19:51 +0530

    perameter pass from url to servlet servlet has to accept input from the client

--- J2EE/src/main/java/com/dolly/servlet/FirstServlet.java
+++ J2EE/src/main/java/com/dolly/servlet/FirstServlet.java
@@ -15,27 +15,18 @@ import javax.servlet.http.HttpServletResponse;
 
 public class FirstServlet extends HttpServlet {
 	
-  
-  @Override
-public void init() throws ServletException {
-	// TODO Auto-generated method stub
-	System.out.println("init call");
-}
   @Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	  System.out.println("service call");
-	 resp.setContentType("text/html");
-		PrintWriter writer = resp.getWriter();
-		writer.println("<h3>hiiiii</h3>");
+	  
+	  String name = req.getParameter("name");
+	  String desg = req.getParameter("desg");
+	  String tech = req.getParameter("techskills");
+	  
+	  System.out.println("name = "+name);
+	  System.out.println("desg = "+desg);
+	  System.out.println("techskills = "+tech);
 		
 	}
-  @Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-	  
-		System.out.println("destroy");
-	}
-
+ 
 
 }
